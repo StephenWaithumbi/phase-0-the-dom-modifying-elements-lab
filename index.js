4e1abdde0cf56d4233e94605aa31557f0525c701
Stephen Waithumbi <stephenwaithumbi14@gmail.com>
2024-10-05 22:44:48 +0300

    this one was kinda tricky

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let noMain = document.querySelector('main');
+noMain.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Steven is the champion"
+document.body.append(newHeader);
+
